03738782ef8591b2445fbc6f3ca30482447bd65e
mackruk <mwilczek@mail.ru>
2015-12-10 04:51:43 +0000

    l&f 2

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,6 +3,14 @@ class PageController < ApplicationController
   end
 
   def service
+    @lis = [
+      'Brak obrazu', 'Odzyskiwanie danych', 'Przywracanie ustawień fabrycznych',
+      'Windows Vista, 7, 8, 10', 'Naprawa telefonów komórkowych',
+      'Transfer danych', 'Usuwanie Spyware', 'iPhone / iPad / iPod',
+      'Naprawa Mac-ów', 'Naprawa Tabletów', 'Instalowanie oprogramowania',
+      'Problemy z drukarką', 'Problemy z ładowaniem', 'Mac OS X',
+      'Upgrade', 'Problemy z Audio'
+    ]
   end
 
   def contact
